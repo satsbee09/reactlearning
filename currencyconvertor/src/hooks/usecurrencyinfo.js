b5a7b91db7440b1a206bc32ad63c526bb3d07fe5
Satsbe09 <satyamsingh4921@gmail.com>
2026-02-07 01:20:27 +0530

    only addition and modification of files, no del

--- currencyconvertor/src/hooks/usecurrencyinfo.js
+++ currencyconvertor/src/hooks/usecurrencyinfo.js
@@ -1,5 +1,15 @@
 import { useEffect,useState } from "react";
 
 function useCurrencyInfo(currenccy){
-  useEffect(()=>{},[])
+    const [data, setdata]=useState({})
+  useEffect(()=>{
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/${currency}.json`)
+    .then((res)=>res.json())
+    .then((res)=>setdata(res[currency]))
+    console.log(data);
+  },[currency])
+  console.log(data);
+  return data;
+
 }
+export default useCurrencyInfo;
