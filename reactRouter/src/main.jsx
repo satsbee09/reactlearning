ec5f94f267a06bfccfc6c25ab2091ca2e59f5df5
Satsbe09 <satyamsingh4921@gmail.com>
2026-02-19 01:16:11 +0530

    layout with outlet and nested routes

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import Contactus from "./components/Contactus/Contactus";
+import Aboutus from "./components/AboutUs/Aboutus";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "aboutus",
+        element: <Aboutus />,
+      },
+      {
+        path: "contactus",
+        element: <Contactus />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
